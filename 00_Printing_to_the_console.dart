b7981fa3a817a76dec8a8757780478d529852b0e
陈 豪嘉 <chenhj@mail.ynu.edu.cn>
2023-04-17 02:28:03 +0000

    00

--- 00_Printing_to_the_console.dart
+++ 00_Printing_to_the_console.dart
@@ -1,6 +1,13 @@
 // Printing to the console：https://dart.cn/guides/libraries/library-tour#printing-to-the-console
 
 void main(){
+  /*
+  The top-level print() method takes a single 
+  argument (any Object) and displays that object’s
+   string value (as returned by toString()) in the console. */
+  String  anObject = 'object';
+  String tea = 'tea';
+  
   print(anObject);
-  print('I drink $tea');
+  print('I drink $tea.');
 }
